98dfefda8b6ebb836432612a039d38a4745a64a8
Sarabjit Singh <websachin111@gmail.com>
2022-10-23 22:39:20 +0530

    add responsiveness

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import "./about.css";
+import { motion } from "framer-motion";
+import principalImg from "../../images/principal.jpg";
+import buildingImg from "../../images/building.jpeg";
 
 export const About = () => {
+  const transition = { duration: 1.5, type: "spring", bounce: 0.35 };
   return (
     <>
-    <h1>About page</h1>
+      <section className="about-sec">
+        <motion.div
+          className="left"
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ ...transition }}
+        >
+          <img src={principalImg} alt="principal img" />
+          <p>
+            Rohit Tandon run this college. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Placeat, eveniet.
+          </p>
+        </motion.div>
+        <div className="right">
+          <motion.p
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ...transition, delay: 0.1 }}
+          >
+            HIMT college is the best college in Hoshiarpur in IT sector. Here
+            you can got your <strong>dream placement</strong>. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Vero pariatur labore itaque,
+            aspernatur temporibus magnam voluptatum illum voluptatem ipsum
+            doloremque impedit fugit quas voluptatibus odit quis, nihil facilis
+            totam eius commodi quaerat eveniet quo. Maiores, eos perspiciatis?
+            Perferendis rem atque vitae a, totam ipsa hic magnam minima quia
+            asperiores, ad ut, vel iusto tempore reiciendis! Vero nesciunt odit,
+            natus provident, saepe alias amet, ab sunt consequuntur sapiente
+            tempore aliquid! Dolores sed porro, accusantium sunt vel nihil non
+            commodi expedita unde.
+          </motion.p>
+          <motion.img
+            src={buildingImg}
+            alt="building img"
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ...transition, delay: 0.2 }}
+          />
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./gallery.css";
+import { motion } from "framer-motion";
+import college1Img from "../../images/college1.jpg";
+import college2Img from "../../images/college2.jpg";
+import college3Img from "../../images/college3.jpg";
+import buildingImg from "../../images/building.jpeg";
 
 export const Gallery = () => {
+  const galleryImages = [
+    { id: 1, image: college1Img },
+    { id: 2, image: college2Img },
+    { id: 3, image: college3Img },
+    { id: 4, image: buildingImg },
+  ];
+  const transition = { duration: 1.5, type: "spring", bounce: 0.35 };
   return (
     <>
-    <h1>Gallery page</h1>
+      <section className="gallery-sec">
+        <h1 className="mid-heading">Gallery</h1>
+        <div className="img-con">
+          {galleryImages.map(({id, image}) => {
+            return <motion.img src={image} alt={image} loading="lazy" key={id}
+            initial={{ y: 100, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ ...transition, delay: `0.${id}` }} />;
+          })}
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
